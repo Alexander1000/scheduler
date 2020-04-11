8939c2ede0532cf4d53e4c391bf1702be4b5a467
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-11 15:46:32 +0300

    add unit-test

---
+++ tests/main.cpp
@@ -0,0 +1,22 @@
+#include <cpp-unit-test.h>
+
+CppUnitTest::TestCase* testSchedule_ValidData_Positive()
+{
+    CppUnitTest::TestCase* t = nullptr;
+    t = new CppUnitTest::TestCase("001-first-integration-test");
+
+    t->printTitle();
+
+    t->finish();
+    return t;
+}
+
+int main() {
+    CppUnitTest::TestSuite testSuite;
+
+    testSuite.addTestCase(testSchedule_ValidData_Positive());
+
+    testSuite.printTotal();
+
+    return 0;
+}
