a314e8c9d8e3cfcbd016c7eb53a6c56561276831
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-11 16:02:46 +0300

    item constructor

--- include/scheduler/item.h
+++ include/scheduler/item.h
@@ -6,12 +6,13 @@
 namespace Scheduler {
     // item
     class Item {
+        Item(int id, std::map<int, int>* resources);
     private:
         // identifier of item
         int id;
 
         // resources for item
-        std::map<int, int> resources;
+        std::map<int, int>* resources;
 
         // identifier of bucket
         int* bucketId;
--- src/scheduler/item.cpp
+++ src/scheduler/item.cpp
@@ -1,1 +1,11 @@
 #include <scheduler.h>
+
+namespace Scheduler
+{
+    Item::Item(int id, std::map<int, int>* resources)
+    {
+        this->id = id;
+        this->resources = resources;
+        this->bucketId = nullptr;
+    }
+}
