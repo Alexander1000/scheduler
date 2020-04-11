27910a25b48b344f72c51664747f1746ee477d2a
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-11 17:46:59 +0300

    update test

--- tests/main.cpp
+++ tests/main.cpp
@@ -37,6 +37,17 @@ CppUnitTest::TestCase* testSchedule_ValidData_Positive()
 
     s.ScheduleItem(&item1);
 
+    for (int i = 0; i < 4; ++i) {
+        std::map<int, int>* itemR2;
+        itemR2 = new std::map<int, int>;
+        itemR2->insert(std::pair<int, int>(rCpu, 9));
+        itemR2->insert(std::pair<int, int>(rMemory, 10000));
+        itemR2->insert(std::pair<int, int>(rGpu, 4));
+        Scheduler::Item* item2;
+        item2 = new Scheduler::Item(i + 1, itemR2);
+        s.ScheduleItem(item2);
+    }
+
     t->finish();
     return t;
 }
