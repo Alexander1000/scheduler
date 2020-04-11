7e9465768a16efc95fffe506e0137730b8097919
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-11 16:05:19 +0300

    init scheduler

--- include/scheduler/scheduler.h
+++ include/scheduler/scheduler.h
@@ -9,15 +9,16 @@
 namespace Scheduler {
     // scheduler
     class Scheduler {
+        Scheduler();
     private:
         // pool of resources
-        std::list<Resource*> resource_pool;
+        std::list<Resource*>* resource_pool;
 
         // pool of buckets
-        std::list<Bucket*> bucket_pool;
+        std::list<Bucket*>* bucket_pool;
 
         // list items for schedule
-        std::list<Item*> pending_items;
+        std::list<Item*>* pending_items;
     };
 }
 
--- src/scheduler/scheduler.cpp
+++ src/scheduler/scheduler.cpp
@@ -1,1 +1,12 @@
 #include <scheduler.h>
+#include <list>
+
+namespace Scheduler
+{
+    Scheduler::Scheduler()
+    {
+        this->resource_pool = new std::list<Resource*>;
+        this->bucket_pool = new std::list<Bucket*>;
+        this->pending_items = new std::list<Item*>;
+    }
+}
