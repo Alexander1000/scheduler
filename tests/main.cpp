4d221e5158a2340d556de214201b6c1c8a6e9685
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-14 08:05:07 +0300

    remove old test case

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,10 +9,6 @@
 
 #include "sequence.cpp"
 
-#define RESOURCE_CPU 1
-#define RESOURCE_MEMORY 2
-#define RESOURCE_GPU 3
-
 class AssertDistributionNotExists
 {};
 
@@ -134,145 +130,6 @@ void assertDistribution(CppUnitTest::TestCase* t, std::map<int, int*>* expectedD
             t->increment();
         }
     }
-}
-
-Scheduler::Item* createItem(int itemId, int cpu, int memory, int gpu)
-{
-    std::map<int, int>* resourceMap;
-    resourceMap = new std::map<int, int>;
-    if (cpu > 0) {
-        resourceMap->insert(std::pair<int, int>(RESOURCE_CPU, cpu));
-    }
-    if (memory > 0) {
-        resourceMap->insert(std::pair<int, int>(RESOURCE_MEMORY, memory));
-    }
-    if (gpu > 0) {
-        resourceMap->insert(std::pair<int, int>(RESOURCE_GPU, gpu));
-    }
-
-    return new Scheduler::Item(itemId, resourceMap);
-}
-
-CppUnitTest::TestCase* testSchedule_ValidData_Positive()
-{
-    CppUnitTest::TestCase* t = nullptr;
-    t = new CppUnitTest::TestCase("001-first-integration-test");
-
-    t->printTitle();
-
-    Sequence sequence;
-
-    // make scheduler
-    Scheduler::Scheduler s;
-
-    for (int i = 0; i < 10; i++) {
-        ResourceMap* resourceMap;
-        resourceMap = new std::map<int, int>;
-        // 64 cpu
-        resourceMap->insert(std::pair<int, int>(RESOURCE_CPU, 64));
-        // 200 Gb memory
-        resourceMap->insert(std::pair<int, int>(RESOURCE_MEMORY, 200000));
-        // 4 gpu
-        resourceMap->insert(std::pair<int, int>(RESOURCE_GPU, 4));
-
-        Scheduler::Bucket* bucket;
-        bucket = new Scheduler::Bucket(i + 1, resourceMap);
-        s.AddBucket(bucket);
-    }
-
-    s.ScheduleItem(createItem(sequence.GetNextID(), 5, 1000, 0));
-
-    for (int i = 0; i < 4; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 9, 10000, 4));
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 10, 10000, 3));
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 10, 10000, 2));
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 10, 10000, 1));
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 5, 30000, 0));
-    }
-
-    s.ScheduleItem(createItem(sequence.GetNextID(), 30, 9000, 0));
-
-    for (int i = 0; i < 5; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 20, 5000, 0));
-    }
-
-    for (int i = 0; i < 15; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 12, 50000, 0));
-    }
-
-    s.ScheduleItem(createItem(sequence.GetNextID(), 20, 90000, 0));
-
-    s.ScheduleItem(createItem(sequence.GetNextID(), 40, 150000, 0));
-
-    s.ScheduleItem(createItem(sequence.GetNextID(), 12, 50000, 0));
-
-    for (int i = 0; i < 15; ++i) {
-        s.ScheduleItem(createItem(sequence.GetNextID(), 3, 5000, 0));
-    }
-
-    // make expected distribution
-    DistributionMap* expectedDistribution;
-    expectedDistribution = new DistributionMap;
-
-    // data-set for bucket #1
-    int items1[10] = {1, 2, 18, 19, 20, 21, 22, 24, 47, 0};
-    expectedDistribution->emplace(1, items1);
-
-    // data-set for bucket #2
-    int items2[5] = {3, 23, 25, 48, 0};
-    expectedDistribution->emplace(2, items2);
-
-    // data-set for bucket #3
-    int items3[6] = {4, 26, 27, 29, 49, 0};
-    expectedDistribution->emplace(3, items3);
-
-    // data-set for bucket #4
-    int items4[8] = {5, 28, 30, 31, 50, 51, 52, 0};
-    expectedDistribution->emplace(4, items4);
-
-    // data-set for bucket #5
-    int items5[8] = {6, 14, 32, 33, 34, 53, 54, 0};
-    expectedDistribution->emplace(5, items5);
-
-    // data-set for bucket #6
-    int items6[8] = {7, 15, 35, 36, 37, 55, 56, 0};
-    expectedDistribution->emplace(6, items6);
-
-    // data-set for bucket #7
-    int items7[8] = {8, 16, 38, 39, 40, 57, 58, 0};
-    expectedDistribution->emplace(7, items7);
-
-    // data-set for bucket #8
-    int items8[8] = {9, 17, 41, 42, 43, 59, 60, 0};
-    expectedDistribution->emplace(8, items8);
-
-    // data-set for bucket #9
-    int items9[7] = {10, 11, 44, 46, 61, 0};
-    expectedDistribution->emplace(9, items9);
-
-    // data-set for bucket #9
-    int items10[4] = {12, 13, 45, 0};
-    expectedDistribution->emplace(10, items10);
-
-    // distribution items in buckets
-    DistributionMap* distribution = s.__GetDistributionItems();
-
-    assertDistribution(t, expectedDistribution, distribution);
-
-    t->finish();
-    return t;
 }
 
 ResourceMap* parseResourcesFromYaml(YamlObject* objResources, ResourceMapDict* dictResources)
@@ -538,7 +395,6 @@ static int filter(const struct dirent* dir_ent)
 int main() {
     CppUnitTest::TestSuite testSuite;
 
-    testSuite.addTestCase(testSchedule_ValidData_Positive());
     std::vector<std::string> v;
 
     struct dirent **namelist;
