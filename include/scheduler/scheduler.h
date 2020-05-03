41581f529d7e3f54e6b0e809cab9266f791783f8
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-05-03 13:25:00 +0300

    update

--- include/scheduler/scheduler.h
+++ include/scheduler/scheduler.h
@@ -9,7 +9,7 @@
 #include <scheduler/strategy.h>
 
 #define SCHEDULE_STATISTIC_ITEMS_COUNT 5
-#define SCHEDULE_STATISTIC_RANDOM_ITEMS_COUNT 10
+#define SCHEDULE_STATISTIC_RANDOM_ITEMS_COUNT 15
 
 // itemID: fill factor
 typedef std::map<int, float> FillFactorMap;
